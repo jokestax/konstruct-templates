3f414794bac4fe80430c89d4b80bee247988eb2e
mrrishi <mrrishi373@gmail.com>
2025-10-21 02:24:52 +0530

    first commit

---
+++ terraform/aws/modules/workload-downstream-vcluster/variables.tf
@@ -0,0 +1,13 @@
+variable "host_cluster" {
+  description = "the name of the host cluster"
+  type        = string
+}
+
+variable "vcluster_name" {
+  description = "the name of the vcluster"
+  type        = string
+}
+
+variable "domain_name" {
+    type = string
+}
